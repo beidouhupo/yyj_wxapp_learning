8cb0442854711b1ef324d9d467d27adb61c50491
beidouhopo <2473502670@qq.com>
2018-05-21 23:38:30 +0800

    动态发布

---
+++ wxapp-starter/config/api.js
@@ -0,0 +1,3 @@
+export const API_BASE = 'https://www.easy-mock.com/mock/5afc2a2fe5c64d22cc1ca4c2/get#!method=get'
+
+export const API_CREATE = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get'
